a391a278a8c813e787e9527dc71ff8008676e484
jjcoop <mrjacobcooper@yahoo.com>
2022-04-13 01:24:15 +1000

    getting set for api calls from front end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+//import { ReactDOM } from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import GlobalStyle from "./components/GlobalStyle";
 import Pages from "./pages";
 
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  uri,
+  cache,
+  connectToDevTools: true,
+});
+
 function App() {
   return (
-    <div>
+    <ApolloProvider client={client}>
       <GlobalStyle />
       <Pages />
-    </div>
+    </ApolloProvider>
   );
 }
 export default App;
